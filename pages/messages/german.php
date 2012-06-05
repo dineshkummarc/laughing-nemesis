20b456e2afdc9d8b4e92ed6d351d7d942d5ed59a
Nico Nägele <nixalio@gmail.com>
2012-06-05 20:25:13 +0200

    license header update

--- pages/messages/german.php
+++ pages/messages/german.php
@@ -1,16 +1,28 @@
 <?PHP
-//	Streamers Admin Panel 3.2
-//	djcrackhome & WallCity-Server Coop.
-//	GNU License
-//	http://www.stremerspanel.com
-///////////////////////////////////////////////
-//	./pages/messages/german.php
-//
-
-//	Translated by: Sebastian Graebner
-//	GERMAN LANGUAGE FILES
-
-
+/**
+ * Streamers Admin Panel 3.2 - Final
+ *
+ * Originally written by Sebastian Graebner <djcrackhome>
+ * Fixed and edited by David Schomburg <dave>
+ *
+ * The Streamers Admin Panel is a web-based administration interface for
+ * Nullsoft, Inc.'s SHOUTcast Distributed Network Audio Server (DNAS),
+ * and is intended for use on the Linux-distribution Debian.
+ *
+ * LICENSE: This work is licensed under the Creative Commons Attribution-
+ * ShareAlike 3.0 Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California,
+ * 94041, USA.
+ *
+ * @author     Sebastian Graebner <djcrackhome@streamerspanel.com>
+ * @author     David Schomburg <dave@streamerspanel.com>
+ * @copyright  2009-2012  S. Graebner <djcrackhome> D. Schomburg <dave>
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported License
+ * @version    3.2 Final
+ * @link       http://www.streamerspanel.com
+ * @since      File available since Release 3.2 public-beta
+ */
 //
 //
 //  ./install.php
